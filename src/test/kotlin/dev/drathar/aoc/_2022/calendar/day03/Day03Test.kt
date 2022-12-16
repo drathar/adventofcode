74a3c09ea49d7933ff2818e28724191d05af4308
Richard Hajdu <richard.hajdu.93@gmail.com>
2022-12-16 18:16:22 +0100

    Solve Advent of Code 2022 - Day 3 (#11)

---
+++ src/test/kotlin/dev/drathar/aoc/_2022/calendar/day03/Day03Test.kt
@@ -0,0 +1,32 @@
+package dev.drathar.aoc._2022.calendar.day03
+
+
+import dev.drathar.aoc._2022.DaggerTestDayComponent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+internal class Day03Test {
+    @Inject
+    lateinit var day03: Day03
+
+    @BeforeEach
+    fun setup() {
+        DaggerTestDayComponent.create().inject(this)
+    }
+
+    @Test
+    fun testDay03PartOne() {
+        assertEquals(7716, day03.partOne(DAY_03))
+    }
+
+    @Test
+    fun testDay03PartTwo() {
+        assertEquals(2973, day03.partTwo(DAY_03))
+    }
+
+    companion object {
+        private const val DAY_03: String = "advent-of-code-input/2022/day03.input"
+    }
+}
