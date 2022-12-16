682a4de675625878bb03c6a36d661d7e833d0f7b
Richard Hajdu <richard.hajdu.93@gmail.com>
2022-12-16 18:08:33 +0100

    Solve Advent of Code 2022 - Day 1 (#9)

--- src/test/kotlin/dev/drathar/aoc/_2022/TestDayComponent.kt
+++ src/test/kotlin/dev/drathar/aoc/_2022/TestDayComponent.kt
@@ -3,10 +3,12 @@ package dev.drathar.aoc._2022
 import dagger.Component
 import dev.drathar.aoc.DayComponent
 import dev.drathar.aoc.InputModule
+import dev.drathar.aoc._2022.calendar.day01.Day01Test
 import javax.inject.Singleton
 
 
 @Singleton
 @Component(modules = [InputModule::class])
 internal interface TestDayComponent : DayComponent {
+    fun inject(day01Test: Day01Test)
 }
