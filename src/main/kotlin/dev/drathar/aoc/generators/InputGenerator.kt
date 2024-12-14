3cf8384ecc664fdac1b8f053dc05087ae7ee179b
drathar <richard.hajdu.93@gmail.com>
2024-12-14 13:07:26 +0100

    Upgrade InputGenerator

--- src/main/kotlin/dev/drathar/aoc/generators/InputGenerator.kt
+++ src/main/kotlin/dev/drathar/aoc/generators/InputGenerator.kt
@@ -2,9 +2,7 @@ package dev.drathar.aoc.generators
 
 import java.io.File
 
-class InputGenerator private constructor(private val text: String) {
-
-    private val lines: Sequence<String> = text.lineSequence()
+class InputGenerator private constructor(private val lines: Sequence<String>) {
 
     fun <Out> readOneLine(singleLineHandler: (String) -> Out) = singleLineHandler(lines.first())
 
@@ -12,19 +10,12 @@ class InputGenerator private constructor(private val text: String) {
 
     fun <In, Out> readLinesAs(converter: (String) -> In, handler: (Sequence<In>) -> Out) = handler(lines.map(converter))
 
-    fun <Out> readBlock(delimiter: String = "\n\n", blockHandler: (Sequence<Sequence<String>>) -> Out) =
-        blockHandler(text.splitToSequence(delimiter).map(String::lineSequence))
-
-    fun <In, Out> readBlockAs(delimiter: String = "\n\n", converter: (String) -> In, blockHandler: (Sequence<Sequence<In>>) -> Out) =
-        blockHandler(text.splitToSequence(delimiter).map { it.lineSequence().map(converter) })
-
     class InputGeneratorFactory {
         fun forFile(fileName: String): InputGenerator {
             val file = File(fileName)
-
-            val text = file.readText()
-
-            return InputGenerator(text)
+            val bufferedReader = file.bufferedReader()
+            val lines = bufferedReader.lineSequence()
+            return InputGenerator(lines)
         }
     }
 }
