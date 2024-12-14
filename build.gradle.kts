a6ec1eaa77b5b745a1f0cf8539de11aec2385592
drathar <richard.hajdu.93@gmail.com>
2024-12-14 12:14:43 +0100

    Delete artifact id

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,6 @@ plugins {
     jacoco
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
